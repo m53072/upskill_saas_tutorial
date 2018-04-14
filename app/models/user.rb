aa62ed8207f0f4dc7fcd2354959cc89f4284b050
Matthew Krueger <mjkrueger23@yahoo.com>
2018-04-14 06:36:38 +0000

    Added association for plans and users

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
          
+  belongs_to :plan
+         
 end
