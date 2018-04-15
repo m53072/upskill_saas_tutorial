ad7ed3482fe8ebff0fb679ae294cc31042887f91
Matthew Krueger <mjkrueger23@yahoo.com>
2018-04-15 04:04:04 +0000

    Added code comments for Stripe

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,11 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
          
   belongs_to :plan
-  
+  # If pro user passes vaildations (email, password, etc), the call Stripe
+  # and tell Stripe to set up a subscription
+  # upon charging a customer's card
+  #Stripe responds back with customer data
+  # Store customer.id as customer token and save user.
   attr_accessor :stripe_card_token
   def save_with_subscription
     if valid?
